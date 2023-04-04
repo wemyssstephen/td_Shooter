235470d888002474923d6c9f49139981e8bb8874
wemys <wemyssstephen@gmail.com>
2023-04-05 00:49:33 +0100

    commit test

---
+++ tiled/RPG Nature Tileset Autumn.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="RPG Nature Tileset Autumn" tilewidth="32" tileheight="32" tilecount="135" columns="15">
+ <image source="../../../Downloads/RPG Nature Tileset Autumn.png" width="480" height="288"/>
+</tileset>
